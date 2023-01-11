23372df280df22f25d5c06abc73fe7ab2ac8c184
Jair Botello <jairbotello25@outlook.com>
2023-01-11 05:15:08 -0600

    conexión firebase

---
+++ js/firebaseConexion.js
@@ -0,0 +1,13 @@
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyAXRhetiIml231RDAm9gQ63ud_reLo4nac",
+    authDomain: "app-gym-sportcentre.firebaseapp.com",
+    projectId: "app-gym-sportcentre",
+    storageBucket: "app-gym-sportcentre.appspot.com",
+    messagingSenderId: "140663075654",
+    appId: "1:140663075654:web:570e54c564d0672de76b47",
+    measurementId: "G-WBQQXKVGJV"
+  };
+
+  const app = initializeApp(firebaseConfig);
